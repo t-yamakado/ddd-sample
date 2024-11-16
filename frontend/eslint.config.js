38de35a75ef2c5702f82ffb1f13253cce9ed97be
t-yamakado <worldtravel811@gmail.com>
2024-11-16 16:25:27 +0900

    add: react-query

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,13 +1,19 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'prettier'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...pluginQuery.configs['flat/recommended'],
+      'prettier',
+    ],
     parser: '@typescript-eslint/parser',
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
